62c777451a76146aab2465a4b649e3988cc89ad8
swagat9712 <swagat1297@gmail.com>
2021-01-27 12:57:44 +0530

    Added: dashboard file

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import LogIn from "./component/logIn/LogIn.jsx";
+import Dashboard from "./component/dashboard/dashboard.jsx"
 import history from "./History.js"; 
 
 export default class Routes extends Component {
@@ -12,6 +13,7 @@ export default class Routes extends Component {
             <Redirect to="/logIn" component={LogIn} />
           </Route>
           <Route exact path="/logIn" component={LogIn} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
     );
