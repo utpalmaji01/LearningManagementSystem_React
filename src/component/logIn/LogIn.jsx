1d43208d8655d71b6fae4974971279c73f6ac38b
Utpal Maji <utpalmaji.um@gmail.com>
2021-01-27 16:20:37 +0530

    add: branch & login page structure

--- src/component/logIn/LogIn.jsx
+++ src/component/logIn/LogIn.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import "./logIn.scss"
+import React, { Component } from "react";
+// import history from "../../History.js";
+import loginImg from "../../assets/image/login.png"
+import "./logIn.scss";
 class LogIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <div className="test">welcome to lms</div> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="login-container">
+        <div className="login-picture"><img src={loginImg} alt="log-in-img" className="login-image" /></div>
+        <div className="login-from"></div>
+      </div>
+    );
+  }
 }
- 
+
 export default LogIn;
