66e8cd6837d316ba929d54e41bde8f023bf801a4
swagat9712 <swagat1297@gmail.com>
2021-01-29 10:18:09 +0530

    Added: student and course file

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import LogIn from "./component/logIn/LogIn.jsx";
 import Dashboard from "./component/dashboard/dashboard.jsx"
+import Student from './component/student/Student.jsx'
+import Course from './component/Course/Course.jsx'
 import history from "./History.js"; 
 
 export default class Routes extends Component {
@@ -14,6 +16,8 @@ export default class Routes extends Component {
           </Route>
           <Route exact path="/logIn" component={LogIn} />
           <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/student" component={Student} />
+          <Route exact path="/Course" component={Course} />
         </Switch>
       </Router>
     );
