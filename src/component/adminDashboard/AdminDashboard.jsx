ec93040d1fd886bca2d112c62cd4e5edb9301f6e
swagat9712 <swagat1297@gmail.com>
2021-02-03 11:58:34 +0530

    Refactored: admin dashboard

--- src/component/adminDashboard/AdminDashboard.jsx
+++ src/component/adminDashboard/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import AppBar from "../appBar/Appbar.jsx";
+import UserProfile from "../userProfile/userProfile.jsx";
 import AdminDetails from "../adminDashboardDetails/AdminDashboardDetails.jsx";
 import "./adminDashboard.scss";
 const Dashboard = () => {
@@ -11,25 +12,17 @@ const Dashboard = () => {
       </div>
       <div className="dashboard-body-admin">
         <div className="dashboard-details-admin">
-          {/* <Switch>
+          <Switch>
             <Route
-<<<<<<< HEAD
               path="/userdashboard/details"
               component={() => <AdminDetails />}
             />
-            <Route
+             <Route
               path="/userdashboard/profile"
-              component={() =><div><h1>user profile</h1></div> }
+              component={() => <UserProfile /> }
             />
           </Switch>
-          {/* <AdminDetails /> */}
-=======
-              path="/admindashboard/details"
-              component={() => <AdminDetails />}
-            />
-          </Switch> */}
-          <AdminDetails />
->>>>>>> dev
+          
         </div>
         
       </div>
